bac6705e0f41d05184e9a951cc0582fa7cd22eef
MarcosGabriel-01 <marcos.castrogiovani@hotmail.com>
2025-06-02 23:29:11 -0300

    pasado a CLI

---
+++ tp/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import Buscador from "./components/Buscador.vue";
+</script>
+
+<template>
+  <Buscador/>
+</template>
+
+<style scoped></style>
---
+++ tp/src/components/Buscador.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container-fluid mt-3">
+    <input
+      type="text"
+      class="form-control"
+      v-model="criterioDeBusqueda"
+      placeholder="Ingresar un criterio de busqueda..."
+    />
+    <div class="card-deck m-0">
+    <div class="row">
+      <div class="col" v-for="persona in personasFiltradas">
+        <div class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">{{ getNombreCompleto(persona) }}</h5>
+            <p class="card-text">dni {{ persona.dni }}</p>
+            <a href="#" class="card-link">{{ persona.correo }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      criterioDeBusqueda: "",
+      personas: [
+        {
+          nombre: "Daniel",
+          apellido: "Sanchez",
+          correo: "danielsanchez68@hotmail.com",
+          dni: "20442873",
+        },
+        {
+          nombre: "Juan",
+          apellido: "Perez",
+          correo: "j@p.gmail.com",
+          dni: "12345678",
+        },
+        {
+          nombre: "Ana",
+          apellido: "Suarez",
+          correo: "a@s.gmail.com",
+          dni: "87654321",
+        },
+        {
+          nombre: "...",
+          apellido: "...",
+          correo: "...",
+          dni: "...",
+        },
+      ],
+    };
+  },
+  computed: {
+    personasFiltradas() {
+      return this.personas.filter((persona) => {
+        let registroCompleto = `${persona.nombre} ${persona.apellido} ${persona.dni} ${persona.correo}`;
+        return registroCompleto
+          .toLowerCase()
+          .includes(this.criterioDeBusqueda.toLowerCase());
+      });
+    },
+  },
+  methods: {
+    getNombreCompleto(persona) {
+      return `${persona.nombre} ${persona.apellido}`;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
